ac92af72b12be116f99b6774753c4237df879ef4
hyperdownload <89705058+hyperdownload@users.noreply.github.com>
2023-11-28 22:55:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function createCards(data) {
+    var container = document.querySelector('.container');
+
+    // Iterate over the JSON data
+    data.forEach(function (card) {
+        // Create card element
+        var newCard = document.createElement('div');
+        newCard.classList.add('card');
+
+        // Add content to the card
+        newCard.innerHTML = `<h2>${card.title}</h2><p>${card.paragraph}</p>`;
+
+        // Add the card to the container
+        container.appendChild(newCard);
+    });
+}
+
+// Fetch data from JSON file
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => createCards(data))
+    .catch(error => console.error('Error fetching data:', error));
