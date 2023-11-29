a8502f0c81dda3bdb3089b7724017933a5ae4a24
hyperdownload <89705058+hyperdownload@users.noreply.github.com>
2023-11-29 13:38:22 -0300

    Add files via upload

--- script/calendario-script.js
+++ script/calendario-script.js
@@ -8,7 +8,7 @@ function createCards(data) {
         newCard.classList.add('card');
 
         // Add content to the card
-        newCard.innerHTML = `<h2>${card.title}</h2><h4>${card.date}</h4><p>${card.paragraph}</p>`;
+        newCard.innerHTML = `<h2>${card.day}</h2>`;
 
         // Add the card to the container
         container.appendChild(newCard);
@@ -16,7 +16,7 @@ function createCards(data) {
 }
 
 // Fetch data from JSON file
-fetch('script/data.json')
+fetch('script/calendario.json')
     .then(response => response.json())
     .then(data => createCards(data))
     .catch(error => console.error('Error fetching data:', error));
